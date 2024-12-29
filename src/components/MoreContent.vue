19eb42ffd2d97469ba9db45f41050c573a22df1a
pengmoxiao <pengmoxiao123@outlook.com>
2024-12-29 14:36:50 +0800

    init

---
+++ src/components/MoreContent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="more-content">
+    欢迎来到我的个人网站！我是Dehou23333，一个热衷于探索多元数字世界的科技爱好者。在这里，我分享自己在Minecraft、Phigros、服务器管理及云计算等领域的经验和见解。从构建复杂的红石机械到优化游戏性能，再到搭建稳定高效的云服务环境，每一个项目都是我对技术热情的见证。我的网站不仅是一个作品展示平台，也是一个学习交流的空间。您将找到关于游戏模组开发、音乐节奏游戏攻略、Linux服务器配置以及阿里云、AWS等云服务平台的实用教程和最新资讯。希望通过这个站点，能与更多志同道合的朋友建立联系，共同探讨技术的乐趣，携手成长。期待您的宝贵意见和建议，让我们一起开启这段充满创意和技术挑战的旅程吧！
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+.more-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  width: 100%;
+  height: 100%;
+}
+</style>
